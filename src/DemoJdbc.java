eae46337775bff478a8677ba94241b755afb31b9
dferro <dferro@nisum.com>
2025-04-23 09:12:28 -0500

    Added DB queries examples

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -14,10 +14,44 @@ public class DemoJdbc {
             String url = "jdbc:postgresql://localhost:5432/demo";
             String username = System.getenv("DB_USER");
             String password = System.getenv("DB_PASSWORD");
+             /* The way to do a query is a statement */
+            String sql = "select name from student where  sid = 1";
+            String sql2 = "select * from student";
 
-//            Class.forName("org.postgresql.Driver");
+//          Class.forName("org.postgresql.Driver");
             Connection con = DriverManager.getConnection(url,username,password);
-
             System.out.println("Connection Established");
+
+            /* Create statement */
+            Statement st = con.createStatement();
+
+            /* Execute statement */
+//            ResultSet rs = st.executeQuery(sql)
+//          String name = rs.getString("name");
+//          System.out.println(name);
+            ResultSet rs = st.executeQuery(sql2);
+
+            while(rs.next()){
+                System.out.print(rs.getInt(1) + " - ");
+                System.out.print(rs.getString(2) + " - ");
+                System.out.println(rs.getInt(3));
+            }
+//            System.out.println(rs.next()); // true
+//            rs.next(); // It points to the first record;
+
+
+
+            /* Close the connection */
+            con.close();
+            System.out.println("Connection Closed");
+
+
+
+            /* The way to do a query is a statement
+            *
+            *
+            */
+
+
     }
 }
